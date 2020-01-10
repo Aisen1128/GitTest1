2f8edc558a1084f3d274c25b91d8e0588466c531
Aisen1128 <48211539+Aisen1128@users.noreply.github.com>
2020-01-10 21:27:03 +0800

    test

---
+++ src/com/nowcoder/Main.java
@@ -0,0 +1,37 @@
+package com.nowcoder;
+
+import sun.awt.windows.WPrinterJob;
+
+import java.lang.reflect.Array;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.*;
+
+
+
+public class Main {
+
+    public static void print(int index, Object obj) {
+        System.out.println(String.format("{%d}, %s",
+                index, obj.toString()));
+    }
+
+    public static void demoCommon(){
+        Random random=new Random();
+        for(int i=0;i<4;i++){
+            print(i,random.nextInt(100));
+        }
+        List<Integer> array= Arrays.asList(new Integer[]{1,2,3,4,5});
+        Collections.shuffle(array);
+        print(5,array);
+        Date date=new Date();
+        print(6,date);
+        print(7,date.getTime());
+        DateFormat df=new SimpleDateFormat("HH:mm:ss yyyy/MM/dd");
+        print(8,df.format(date));
+        print(9,UUID.randomUUID());
+    }
+    public static void main(String[] args) {
+	    demoCommon();
+    }
+}
